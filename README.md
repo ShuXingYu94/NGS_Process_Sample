b1750718598ddbd291c9832abc9239f2c16b978b
ShuXingYu94 <57697835+ShuXingYu94@users.noreply.github.com>
2022-12-30 17:36:32 +0900

    update

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Sample code for NGS processing
-This programme takes advantage of Stacks to analyze NGS BaseCall data.
+********************************
+> This programme takes advantage of Stacks to analyze NGS BaseCall data.
+
+1. [Required environment](#Required environment)
+2. [Installing Prerequisites](#Installing Prerequisites)
+3. [Quick Guide](#Quick Guide)
+   1. [Working directory](#1.Working directory)
+   2. [Download shell scripts and configuration](#2.Download shell scripts and configuration)
+   3. [Executing script](#3.Executing script)
+4. [Standard Procedure](#Standard Procedure)
+5. [Interpreting Results](#Interpreting Results)
+
 ## Required environment
-### Linux:
+***
+### Shell:
 - stacks
 - vcftools
 - samtools
@@ -15,3 +27,125 @@ This programme takes advantage of Stacks to analyze NGS BaseCall data.
 - pandas
 - scipy
 - numpy
+- etc.
+
+## Installing Prerequisites
+***
+### For shell package(s):
+
+    Please refer to the homepage of each package for more information.
+
+### For R package(s):
+
+    Run `install.packages("package_needed")` in R.
+
+### For Python package(s):
+
+    Run the following script in shell. This script will download requirements.txt and automatically install.
+
+```
+if command -v wget >/dev/null 2>&1; then
+  wget -O ./requirements.txt https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/requirements.txt
+else
+  if command -v curl >/dev/null 2>&1; then
+    curl -o ./requirements.txt https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/requirements.txt
+  else
+    echo "Wget and Curl are not installed. Please install."
+    exit 0
+  fi
+fi
+
+if command -v pip3 >/dev/null 2>&1; then
+  pip3 install -r requirements.txt
+else
+  if command -v pip >/dev/null 2>&1; then
+    pip install -r requirements.txt
+  else
+    echo "Something wrong with pip/pip3. Please install python packages with other software like conda."
+    exit 0
+  fi
+fi
+
+rm ./requirements.txt
+```
+
+## Quick Guide
+***
+### 1.Working directory
+Please start with the following folder structure.
+```
+work_dir/
+├── BaseCall
+│   ├── sample1_R1_001.fastq.gz
+│   ├── sample1_R2_001.fastq.gz
+│   ├── sample2_R1_001.fastq.gz
+│   └── sample2_R2_001.fastq.gz
+├── MIGadapter.fasta → (Optional)
+└── popmap.txt → (Optional)
+```
+Normally, Put NGS data files in `./work_dir/BaseCall` is all you need to do.
+
+>- For trimming of the original data files, an adapter.fasta file is needed. By default, a `MIGadapter.fasta` file will be downloaded.
+>
+>- In case of multiple population analysis, you can put a `popmap.txt` file in `./work_dir/`. By default, a `popmap.txt` file will be automatically generated with all the samples recognized in the same population.
+
+### 2.Download shell scripts and configuration
+
+- Download `main.sh` and `statistics.sh` files from the Master branch to your working directory.
+
+- Input the required configurations in `main.sh` as follows.
+    ```
+    # Required Settings
+    
+    workdir=/Users/.../work_dir
+    mapping_db=/Users/.../reference_genome.fa
+    trimmomatic_dir=/Users/.../Trimmomatic-0.39/trimmomatic-0.39.jar
+    adapter=${workdir}/MIGadapter.fasta
+    popmap_dir=${workdir}/popmap.txt
+    
+    # Read file names
+    
+    files="d1-LE
+    d2-LE"
+    ```
+- Optional configurations
+
+    In case of analyzing more than 2 samples, it is recommended to set `stacks_r = 0.8` rather than `stacks_r = 1.0` by default.
+> For more information, please refer to the documentation of Trimmomatic and Stacks.
+
+### 3.Executing script
+Please execute the following commands in the same directory as `main.sh` and `statistics.sh`,
+
+```
+./main.sh
+./statistics.sh
+```
+
+or simply copy all the commands and execute them.
+
+## Standard Procedure
+***
+### 1.Working directory
+Please start with the following folder structure.
+
+```
+work_dir/
+├── BaseCall
+│   ├── sample1_R1_001.fastq.gz
+│   ├── sample1_R2_001.fastq.gz
+│   ├── sample2_R1_001.fastq.gz
+│   └── sample2_R2_001.fastq.gz
+├── MIGadapter.fasta → (Optional)
+└── popmap.txt → (Optional)
+```
+Put NGS data files in `./work_dir/BaseCall` is all you need to do.
+
+For trimming of the original data files, an adapter.fasta file is needed. By default, a `MIGadapter.fasta` file will be downloaded.
+
+In case of multiple population analysis, you can put a `popmap.txt` file in `./work_dir/`. By default, a `popmap.txt` file will be automatically generated with all the samples recognized in the same population.
+
+>**...still in progress, please follow the steps in Quick Guide.**
+
+## Interpreting Results
+***
+
--- statistics.sh
+++ statistics.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 # Statistics
-cd ${workdir}
+# Create folder and prerequisites
 touch ${stats_dir}/results.txt
+mkdir ${stats_dir}/Coverage
+mkdir ${stats_dir}/bam2txt
+ls ${aligned_dir}/*.bam | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'> ${stats_dir}/file_names.txt
 
 # Reads Count (Raw Data) - with seqkit stats
 cd  ${basecall_dir}
@@ -13,30 +18,10 @@ echo "#Timmed Reads Count" >> ${stats_dir}/results.txt
 seqkit stats *R[1-2]p*.gz -T -j ${threads} >> ${stats_dir}/results.txt
 cd  ${workdir}
 
-# Trimming rate/ Surviving rate
+# Fetch requeired data files
 
-
-# Mapped Length (After Trimming) - with samtools depth
-#echo "#Mapped Length" >> ${stats_dir}/results.txt
-#mkdir ${stats_dir}/Read_depth
-#for file in $files
-#do
-#samtools depth ${aligned_dir}/$file.bam | awk '($3+0) > 0 {print $0}' > ${stats_dir}/Read_depth/$file.txt
-#echo -e "$file\tMapped Length\t"`awk "END{print NR}" ${stats_dir}/Read_depth/$file.txt` >> ${stats_dir}/results.txt
-##awk 'BEGIN{sum=0}{sum += $3}END{print sum/NR}' ${stats_dir}/Read_depth/$file.txt >> ${stats_dir}/results.txt
-#done
-
-#echo "#Mapped Length with depth >= 10" >> ${stats_dir}/results.txt
-#mkdir ${stats_dir}/Read_depth_10
-#for file in $files
-#do
-#samtools depth ${aligned_dir}/$file.bam | awk '($3+0) >= 10 {print $0}' > ${stats_dir}/Read_depth/$file.txt
-#echo -e "$file\tMapped Length\t"`awk "END{print NR}" ${stats_dir}/Read_depth/$file.txt` >> ${stats_dir}/results.txt
-#done
 
 # Mapped Reads Coverage (After Trimming/ Genome Size)
-        #ls ${aligned_dir}/*.bam > ${stats_dir}/file_names.txt
-mkdir ${stats_dir}/Coverage
 echo "#Coverage" >> ${stats_dir}/results.txt
 echo -e "file\tmean_coverage\tdepth_mean\tread_count\tmapped_length\treference_length" >> ${stats_dir}/results.txt
 for file in $files
@@ -50,16 +35,7 @@ COVERAGE=`awk 'BEGIN{len=0; ref=0}{len += $5; ref += $3}END{print len/ref}' ${st
 echo -e "$file\t$COVERAGE\t$DEPTH_mean\t$READ_sum\t$MAPPED_bases\t$REF_length" >> ${stats_dir}/results.txt
 done
 
-# Average Mapped Reads depth - with samtools depth
-# From above file
-
 # Consensus Length - python script
-#   awk '{print $1 $2}' ${stats_dir}/Read_depth/d1-LE.txt > ${stats_dir}/Read_depth/d1-LE_ref.txt
-#      #awk '{print $1 $2}' ${stats_dir}/Read_depth/d2-LE.txt > ${stats_dir}/Read_depth/d2-LE_ref.txt
-#      #grep -wf ${stats_dir}/Read_depth/d1-LE_ref.txt ${stats_dir}/Read_depth/d2-LE_ref.txt
-ls ${aligned_dir}/*.bam | awk -F "/" '{print $NF}' | awk -F "." '{print $1}'> ${stats_dir}/file_names.txt
-
-mkdir ${stats_dir}/bam2txt
 for file in $files
 do
 if ! test -f "${aligned_dir}/${file}.txt"; then
@@ -67,16 +43,19 @@ if ! test -f "${aligned_dir}/${file}.txt"; then
 fi
 done
 
-if command -v python3 >/dev/null 2>&1; then
-  python3 ${workdir}/consensus.py
-else
-  if command -v python >/dev/null 2>&1; then
-    python ${workdir}/consensus.py
+if ! test -f "${aligned_dir}/${file}.txt"; then
+  if command -v python3 >/dev/null 2>&1; then
+    python3 ${workdir}/consensus.py
   else
-    echo "Python is not installed. Please install."
+    if command -v python >/dev/null 2>&1; then
+      python ${workdir}/consensus.py
+    else
+      echo "Python is not installed. Please install."
+    fi
   fi
+else
+  echo "File 'compare_output.csv' already exists. Skip pythons script 'consensus.py'. "
 fi
-
 # Consensus Coverage (Of Genome Size) - with bash script
 # Consensus Coverage (Of Mapped Length After Trimming)) - with bash script
 
@@ -93,7 +72,6 @@ echo -e "$file\t$CONSENSUS_length\t$CONSENSUS_coverage_G\t$CONSENSUS_coverage_M"
 done
 
 # SNPs Count - with bash script using vcf file
-
 awk -v FS=":" '$8 > 0{ print $0 }' ${workdir}/stacks/populations.snps.vcf | echo -e "SNPs_all\t"`awk "END{print NR}"` >> ${stats_dir}/results.txt
 awk -v FS=":" '$8 > 10{ print $0 }' ${workdir}/stacks/populations.snps.vcf | awk -v FS=":" '$12 > 10{ print $0 }' | echo -e "SNPs_depth>10\t"`awk "END{print NR}"` >> ${stats_dir}/results.txt
 awk -v FS=":" '$8 > 10{ print $0 }' ${workdir}/stacks/populations.snps.vcf | awk -v FS=":" '$12 > 10{ print $0 }'| sed '1s/^/#CHROM\tPOS\n/' > ${stacks_dir}/snps_depth_10.txt
@@ -118,6 +96,9 @@ SNP=$(awk -v FS=":" '$8 > 10{ print $0 }' ${workdir}/stacks/populations.snps.vcf
 SNP_Genome=$(printf "%.1f" $(echo "scale=10;${REF_length}/${SNP}" | bc))
 SNP_Consensus=$(printf "%.1f" $(echo "scale=10;${CONSENSUS_length}/${SNP}" | bc))
 echo -e "10\t$SNP\t$SNP_Genome\t$SNP_Consensus" >> ${stats_dir}/results.txt
+
+
+# Get figures
 
 # Average snp depth - Figure with vcftools - R
 #vcftools --vcf ${stacks_dir}/populations.snps.vcf --out ${stats_dir}/mean_depth_stat --site-mean-depth
