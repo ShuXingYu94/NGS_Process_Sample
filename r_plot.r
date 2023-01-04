0219ceee9ae5a192d8025ead1416e84daf4d738f
ShuXingYu94 <57697835+ShuXingYu94@users.noreply.github.com>
2023-01-04 16:34:38 +0900

    update

--- r_plot.r
+++ r_plot.r
@@ -1,3 +1,3 @@
 library(CMplot);
 data<-read.table(file = "./tmp.txt",header = TRUE);
-CMplot(data,plot.type="m",cex=0.8, band=0.5,col=c("grey30","grey60"),threshold = 10,threshold.lty = 1,LOG10=FALSE,ylab = "Average Depth of SNP", file="jpg", memo="depth", dpi=300, file.output=TRUE, verbose=TRUE);
+CMplot(data,plot.type="m",cex=0.8, band=0.5,col=c("grey30","grey60"),threshold = 10,threshold.lty = 1,LOG10=TRUE,ylab = "Average Depth of SNP", file="jpg", memo="depth", dpi=300, file.output=TRUE, verbose=TRUE);
