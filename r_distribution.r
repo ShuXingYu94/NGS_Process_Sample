55cfabbdcb73de1ed17c2c35d1b96d8c6ae33c41
ShuXingYu94 <57697835+ShuXingYu94@users.noreply.github.com>
2023-01-06 17:40:15 +0900

    Can now process multiple files

--- r_distribution.r
+++ r_distribution.r
@@ -1,3 +1,3 @@
 library(CMplot);
 data<-read.table(file = "./tmp.txt",header = TRUE);
-CMplot(data ,type="p",plot.type="d",bin.size=1e3,chr.den.col=c("grey","black","red"),file="jpg",memo="",dpi=300,main="",file.output=TRUE,verbose=TRUE,width=9,height=6)
+CMplot(data,type="p",plot.type="d",bin.size=1e3,chr.den.col=c("grey","black","red"),file="jpg",memo="",dpi=300,main="",file.output=TRUE,verbose=TRUE,width=9,height=6)
