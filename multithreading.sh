5ee2b7554cb02fbd0e7e6abbfb3d29400de15efd
ShuXingYu94 <57697835+ShuXingYu94@users.noreply.github.com>
2023-01-10 16:08:41 +0900

    Added genome border in distribution figure

--- multithreading.sh
+++ multithreading.sh
@@ -78,15 +78,24 @@ done
 gstacks -I ${aligned_dir} -M ${popmap_dir}  -O ${stacks_dir} -t ${threads}
 populations -t ${threads} -P ${stacks_dir} -M ${popmap_dir} --structure --vcf  -r ${stacks_r} -O ${stacks_dir}
 
+#!/bin/sh
+
+# Settings
+draw_genome_border="FALSE" # FALSE if genoome border is not needed in distribution figure.
+
 # Statistics
 export SNPs_depth
 
 # Download python and r scripts
+cd ${workdir}
 if command -v wget >/dev/null 2>&1; then
   wget -O ${workdir}/r_plot.r https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/r_plot.r
+  wget -O ${workdir}/r_distribution.r https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/r_distribution.r
+
 else
   if command -v curl >/dev/null 2>&1; then
     curl -o ${workdir}/r_plot.r https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/r_plot.r
+    curl -o ${workdir}/r_distribution.r https://raw.githubusercontent.com/ShuXingYu94/NGS_Process_Sample/master/r_distribution.r
   else
     echo "Wget and Curl are not installed. Please install."
     exit 0
@@ -125,11 +134,11 @@ if ! test -f "${stats_dir}/Consensus/consensus.txt"; then
   for chr in $chromosomes
   do
     {
-      cat ${stats_dir}/Read_depth/*.txt | grep $chr | awk '{print $2}' | sort | uniq -c > ${log_dir}/consensus_${chr}.txt
-      CONSENSUS_length=`awk '$1 >= ENVIRON["filter_depth"]{ print $0 }' ${log_dir}/consensus_${chr}.txt | awk "END{print NR}"`
-      map_length=`awk 'END{print NR}' ${log_dir}/consensus_${chr}.txt`
-      echo "$chr" "${CONSENSUS_length}" "$map_length" >> ${stats_dir}/Consensus/tmp_consensus.txt
-      } &
+    cat ${stats_dir}/Read_depth/*.txt | grep $chr | awk '{print $2}' | sort | uniq -c > ${log_dir}/consensus_${chr}.txt
+    CONSENSUS_length=`awk '$1 >= ENVIRON["filter_depth"]{ print $0 }' ${log_dir}/consensus_${chr}.txt | awk "END{print NR}"`
+    map_length=`awk 'END{print NR}' ${log_dir}/consensus_${chr}.txt`
+    echo "$chr" "${CONSENSUS_length}" "$map_length" >> ${stats_dir}/Consensus/tmp_consensus.txt
+    } &
   done
   wait
   sort ${stats_dir}/Consensus/tmp_consensus.txt > ${stats_dir}/Consensus/consensus.txt
@@ -140,7 +149,7 @@ else
 fi
 
 # Main process
-echo -e "file\tR1_reads\tR2_reads\tR1_trimmed_reads\tR2_trimmed_reads\tR1_trim_rate\tR2_trim_rate\tTotal_trim_rate\tmean_coverage\tdepth_mean\tmapped_length\treference_length\tconsensus_length\tconsensus_coverage(Genome)\tconsensus_coverage(Mapped)" > ${stats_dir}/results.txt
+echo -e "file\tR1_reads\tR2_reads\tR1_trimmed_reads\tR2_trimmed_reads\tR1_trim_rate\tR2_trim_rate\tTotal_trim_rate\tmean_coverage\tdepth_mean\tmapped_length\total_mapped_length\treference_length\tconsensus_length\tconsensus_coverage(Genome)\tconsensus_coverage(Mapped)" > ${stats_dir}/results.txt
 for file in $files
 do
   echo "Analyzing file ${file}."
@@ -184,20 +193,21 @@ do
   # Consensus Coverage (Of Genome Size) - with bash script
   # Consensus Coverage (Of Mapped Length After Trimming)) - with bash script
   CONSENSUS_length=`awk 'BEGIN{sum=0}{sum += $2}END{print sum}' ${stats_dir}/Consensus/consensus.txt`
+  total_mapped_length=`awk 'BEGIN{sum=0}{sum += $3}END{print sum}' ${stats_dir}/Consensus/consensus.txt`
   CONSENSUS_coverage_G=$(printf "%.5f" $(echo "scale=10;${CONSENSUS_length}/${REF_length}" | bc))
-  CONSENSUS_coverage_M=$(printf "%.5f" $(echo "scale=10;${CONSENSUS_length}/${MAPPED_bases}" | bc))
+  CONSENSUS_coverage_M=$(printf "%.5f" $(echo "scale=10;${CONSENSUS_length}/${total_mapped_length}" | bc))
 
   echo "Consensus analysis data parsed."
 
-  echo -e "$file\t$R1_reads\t$R2_reads\t$R1_trimmed_reads\t$R2_trimmed_reads\t$R1_trim_rate\t$R2_trim_rate\t$Total_trim_rate\t$COVERAGE\t$DEPTH_mean\t$MAPPED_bases\t$REF_length\t$CONSENSUS_length\t$CONSENSUS_coverage_G\t$CONSENSUS_coverage_M" >> ${stats_dir}/results.txt
+  echo -e "$file\t$R1_reads\t$R2_reads\t$R1_trimmed_reads\t$R2_trimmed_reads\t$R1_trim_rate\t$R2_trim_rate\t$Total_trim_rate\t$COVERAGE\t$DEPTH_mean\t$MAPPED_bases\t$total_mapped_length\t$REF_length\t$CONSENSUS_length\t$CONSENSUS_coverage_G\t$CONSENSUS_coverage_M" >> ${stats_dir}/results.txt
 done
 
 # SNPs Count - with bash script using vcf file
 # Mapped Length/SNP
 # Consensus Length/SNP
 
-awk '/#CHROM/{print $0}' ${workdir}/stacks/populations.snps.vcf > ${stacks_dir}/snps_depth_10.txt
-awk -v FS=":" '$8 >= ENVIRON["SNPs_depth"]{ print $0 }' ${workdir}/stacks/populations.snps.vcf | awk -v FS=":" '$12 >= ENVIRON["SNPs_depth"]{ print $0 }' >> ${stacks_dir}/snps_depth_10.txt
+awk '/#CHROM/{print $0}' ${workdir}/stacks/populations.snps.vcf > ${stacks_dir}/snps_depth_${SNPs_depth}.txt
+awk -v FS=":" '$8 >= ENVIRON["SNPs_depth"]{ print $0 }' ${workdir}/stacks/populations.snps.vcf | awk -v FS=":" '$12 >= ENVIRON["SNPs_depth"]{ print $0 }' >> ${stacks_dir}/snps_depth_${SNPs_depth}.txt
 
 echo -e "\n#SNP count" >> ${stats_dir}/results.txt
 echo -e "SNP_depth\tSNP_count\tgenome_length_per_SNP\tconsensus_length_per_SNP" >> ${stats_dir}/results.txt
@@ -217,47 +227,53 @@ echo -e "SNPs depth: $SNPs_depth\t$SNP\t$SNP_Genome\t$SNP_Consensus" >> ${stats_
 
 # Get figures
 # Average snp depth - Figure with vcftools - R
+# snp across chr - R
 # Whithout filtering
 cd ${stacks_dir}
 vcftools --vcf populations.snps.vcf --out mean_depth_stat --site-mean-depth
 awk '{$4="";print $0}' mean_depth_stat.ldepth.mean |  awk '$0=NR" "$0' > SNP_Mean_Depth.txt
 cd ${workdir}
 cp ./stacks/SNP_Mean_Depth.txt tmp.txt
+if ! draw_genome_border="FALSE";then
+  awk 'NR>1{print $2,$1,$3,0}' ./statistics/Coverage/d1-LE.txt >> tmp.txt # add the right border
+fi
 R -f r_plot.r
+R -f r_distribution.r
 rm tmp.txt
 if test -f "Rectangular-Manhattan.MEAN_DEPTH_depth.jpg"; then
   mv Rectangular-Manhattan.MEAN_DEPTH_depth.jpg ./statistics/SNP_Depth_0.jpg
 else
-  echo "Rscript was not successfully executed."
+  echo "SNP_Depth_0.jpg was not successfully created."
 fi
+if test -f "SNP-Density.MEAN_DEPTH.jpg"; then
+  mv SNP-Density.MEAN_DEPTH.jpg ./statistics/SNPs_Distribution_0.jpg
+else
+  echo "SNPs_Distribution_0.jpg was not successfully created."
+fi
+
 
 # With filtering
 cd ${stacks_dir}
 cat populations.snps.vcf | grep -E '#' > vcfform.txt
-sed -e '/#CHROM/r vcfform.txt' snps_depth_10.txt | sed '1d' > snps_depth_10.vcf
+sed -e '/#CHROM/r vcfform.txt' snps_depth_${SNPs_depth}.txt | sed '1d' > snps_depth_${SNPs_depth}.vcf
 rm vcfform.txt
-vcftools --vcf snps_depth_10.vcf --site-mean-depth --out mean_depth_stat_10
-awk '{$4="";print $0}' mean_depth_stat_10.ldepth.mean |  awk '$0=NR" "$0' > SNP_Mean_Depth_10.txt
+vcftools --vcf snps_depth_${SNPs_depth}.vcf --site-mean-depth --out mean_depth_stat_${SNPs_depth}
+awk '{$4="";print $0}' mean_depth_stat_${SNPs_depth}.ldepth.mean |  awk '$0=NR" "$0' > SNP_Mean_Depth_${SNPs_depth}.txt
 cd ${workdir}
-cp ./stacks/SNP_Mean_Depth_10.txt tmp.txt
+cp ./stacks/SNP_Mean_Depth_${SNPs_depth}.txt tmp.txt
+if ! draw_genome_border="FALSE";then
+  awk 'NR>1{print $2,$1,$3,0}' ./statistics/Coverage/d1-LE.txt >> tmp.txt # add the right border
+fi
 R -f r_plot.r
+R -f r_distribution.r
 rm tmp.txt
 if test -f "Rectangular-Manhattan.MEAN_DEPTH_depth.jpg"; then
-  mv Rectangular-Manhattan.MEAN_DEPTH_depth.jpg ./statistics/SNP_Depth_10.jpg
+  mv Rectangular-Manhattan.MEAN_DEPTH_depth.jpg ./statistics/SNP_Depth_${SNPs_depth}.jpg
 else
-  echo "Rscript was not successfully executed."
+  echo "SNP_Depth_${SNPs_depth}.jpg was not successfully created."
 fi
-
-# snp across chr - python script
-cd ${workdir}
-if command -v python3 >/dev/null 2>&1; then
-  python3 ${workdir}/chr_fig.py ${stacks_dir}/populations.snps.vcf ${stats_dir}/SNPs_Distribution_0
-  python3 ${workdir}/chr_fig.py ${stacks_dir}/snps_depth_10.txt ${stats_dir}/SNPs_Distribution_10
+if test -f "SNP-Density.MEAN_DEPTH.jpg"; then
+  mv SNP-Density.MEAN_DEPTH.jpg ./statistics/SNPs_Distribution_${SNPs_depth}.jpg
 else
-  if command -v python >/dev/null 2>&1; then
-    python ${workdir}/chr_fig.py ${stacks_dir}/populations.snps.vcf ${stats_dir}/SNPs_Distribution_0
-    python ${workdir}/chr_fig.py ${stacks_dir}/snps_depth_10.txt ${stats_dir}/SNPs_Distribution_10
-  else
-    echo "Python is not installed. Please install."
-  fi
+  echo "SNPs_Distribution_${SNPs_depth}.jpg was not successfully created."
 fi
